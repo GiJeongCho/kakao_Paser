bd9654688de622212c7921650c24f3188139b3fa
GiJeongCho <wzxcv123@naver.com>
2026-06-11 16:21:14 +0900

    카카오톡 실행기

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 780,
+    minWidth: 640,
+    minHeight: 480,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
